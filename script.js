f38a48bf49229b6d5c4f830a7775810044cffe6a
Matt <mvaden08@gmail.com>
2016-03-23 14:22:26 -0400

    Added functions on second through sixth id tags. Still working on 'multiple clicks' function for sixth tag. Squashed bugs pending.

---
+++ script.js
@@ -0,0 +1,49 @@
+var clickMe = document.querySelector('#first_paragraph');
+clickMe.addEventListener('click', function (){
+  console.log('h1 got clicked !!!!');
+  document.body.style.background = "blue";
+})
+
+var clickMe = document.querySelector('second_paragraph');
+clickMe.addEventListener('click', function (){
+  console.log('h2 got clicked!!!!');
+  document.body.style.background = "pink";
+})
+
+var clickMe = document.querySelector('first_chorus');
+clickMe.addEventListener('click', function (){
+  console.log('h3 got clicked!!!!');
+  document.body.style.background = "brown";
+})
+
+var clickMe = document.querySelector('third_paragraph');
+clickMe.addEventListener('click', function (){
+  console.log('h4 got clicked!!!!');
+  document.body.style.background = "red";
+})
+
+var clickMe = document.querySelector('fourth_paragraph');
+clickMe.addEventListener('click', function (){
+  console.log('h5 got clicked!!!!');
+  document.body.style.background - "green";
+})
+
+var clickMe = document.querySelector('second_chorus');
+clickMe.addEventListener('click', function (){
+  console.log('h6 got clickec, six times!!!!!!');
+  document.body.style.background - "orange";
+})
+
+//       clickMe.classList.add('party')
+// });
+// var anchor = document.querySelector('a')
+// // line 14 for google.com
+// command shift P > auto indent
+// // Ids use #, classes use . (dots)
+// anchor.addEventListener ('click', function(){
+//       event.preventDefault();
+//       // Don't let us go to Google. Instead, anchor.click.
+//       console.log ('anchor got clicked !!!!!');
+// });
+// // contine on!
+//
